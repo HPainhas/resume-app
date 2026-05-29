2b051be9ed84cbd0e9f456eb6c28cf0ac1cd3efc
henrique-painhas <hpainhas@gmail.com>
2026-05-28 17:11:25 -0700

    Update footer year to 2026

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,28 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/fontawesome-free-solid";
 
 const Footer = () => {
-	return (
-		<Fragment>
-			<footer>
-				<section id="footer" className="section-container">
-					<div className="scroll-up">
-						<a className="smooth-scroll" href="#home">
-							<FontAwesomeIcon icon={faChevronUp} size="2x" />
-						</a>
-					</div>
-					<div className="social-container">
-						<div className="top-10">
-							<span className="barely-readable">
-								&copy; 2023 by{" "}
-							</span>
-							<span className="full-name">Henrique Painhas</span>.
-							All rights reserved.
-						</div>
-					</div>
-				</section>
-			</footer>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <footer>
+        <section id="footer" className="section-container">
+          <div className="scroll-up">
+            <a className="smooth-scroll" href="#home">
+              <FontAwesomeIcon icon={faChevronUp} size="2x" />
+            </a>
+          </div>
+          <div className="social-container">
+            <div className="top-10">
+              <span className="barely-readable">&copy; 2026 by </span>
+              <span className="full-name">Henrique Painhas</span>. All rights
+              reserved.
+            </div>
+          </div>
+        </section>
+      </footer>
+    </Fragment>
+  );
 };
 
 export default Footer;
